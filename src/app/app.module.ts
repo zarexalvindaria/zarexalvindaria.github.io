14c4b8017085f0987eced182070cdca89f110ac5
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-12-29 16:29:30 +0800

    Revert "Divide sections into separate components and Improve color scheme"
    This reverts commit 9bf33182a1b88c02cc83e7488d7d45cf3ca3ab3d.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { EducationComponent } from './education/education.component';
-import { FooterComponent } from './footer/footer.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ProjectsComponent,
-    EducationComponent,
-    FooterComponent
-    
+    HeaderComponent
   ],
   imports: [
     BrowserModule
