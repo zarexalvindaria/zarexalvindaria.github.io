7f6b36d15bd54b00ae73c1f1710e28d691c9dd3d
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-12-30 07:39:09 +0800

    Replace placeholder images with actual project images

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-project',
-  // templateUrl: './project.component.html',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent /*implements OnInit*/ {
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+export class ProjectComponent implements OnInit {
+  images = [`techtrends-cicd`, `udaconnect-preview`, `observability-dashboard`, 
+            `hardened-microservice-monitor`, `techtrends-github-actions`].map((n) => `assets/images/${n}.png`);
 
-  /*constructor() { }
+  constructor() { }
 
   ngOnInit(): void {
-  }*/
+  }
 
 }
