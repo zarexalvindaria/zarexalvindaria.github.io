95696bad478ae7d7dce87f9b7a26c9ddaf7cceb8
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-12-31 05:21:37 +0800

    fix: Downgrade to bootstrap 4.6.1
    The downgrade solves issue with incompatibility of ng-bootstrap's
    carousel with bootstrap version 5.

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
+  providers: [NgbCarouselConfig],
   styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent implements OnInit {
+  techtrends = ['github-actions', 'cicd'].map((n) => `assets/images/techtrends/${n}.png`);
+  udaconnect = [].map((n) => 'assets/images/udaconnect/${n}.png');
+  observability = [].map((n) => 'assets/images/observability/${n}.png');
+  msecurity = [].map((n) => 'assets/images/msecurity/${n}.png');
   images = [`techtrends-cicd`, `udaconnect-preview`, `observability-dashboard`, 
             `hardened-microservice-monitor`, `techtrends-github-actions`].map((n) => `assets/images/${n}.png`);
 
             
-  constructor() { 
+  constructor() {
   }
 
   ngOnInit(): void {
