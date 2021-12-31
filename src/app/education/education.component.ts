fa9c8267c9fe811fb4aa9aa4d032bec357c3bdcd
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-01-01 01:00:26 +0800

    Apply directive *ngFor to list Certifications

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -3,25 +3,55 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.scss']
+  styleUrls: ['./education.component.scss'],
 })
 export class EducationComponent implements OnInit {
-
   @Input() schoolName = '';
   @Input() schoolAddress = '';
   @Input() course = '';
-  @Input() major  = '';
-  @Input() graduation  = '';
+  @Input() major = '';
+  @Input() graduation = '';
 
-  relatedCoursework = ["Database Management", "Mobile & Mobile Programming", 
-                      "Data Structures", "UI/UX Design", "Web Programming", 
-                      "Information Security", "Software Development",
-                      "Systems & Management", "Programming Embedded Systems", 
-                      "Systems and Design", "IT Analytics"];
+  relatedCoursework = [
+    'Database Management',
+    'Mobile & Mobile Programming',
+    'Data Structures',
+    'UI/UX Design',
+    'Web Programming',
+    'Information Security',
+    'Software Development',
+    'Systems & Management',
+    'Programming Embedded Systems',
+    'Systems and Design',
+    'IT Analytics',
+  ];
 
-  constructor() { }
+  certificates = [
+    {
+      certificateName: 'Cloud Native Application Architect Nanodegree',
+      provider: 'Udacity',
+      dateAcquired: '11.2021',
+      credentialID: 'Credential ID HZJUTKXR',
+      verificationURL: 'https://graduation.udacity.com/confirm/HZJUTKXR',
+    },
+    {
+      certificateName: 'Associate Reactive Developer (OutSystems 11)',
+      provider: 'OutSystems',
+      dateAcquired: '04.2021',
+      credentialID: '',
+      verificationURL: '',
+    },
+    {
+      certificateName: 'AWS Certified Cloud Practitioner (CLF)',
+      provider: 'Amazon Web Services',
+      dateAcquired: '03.2021',
+      credentialID: 'Credential ID 4S4E4TKBK2V4Q19X',
+      verificationURL:
+        'https://www.youracclaim.com/badges/da75d76f-315c-4365-bfff-f5889234ccf4',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
