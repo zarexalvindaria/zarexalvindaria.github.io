73902b64366bc1968c7ff951443f4ea110cf360d
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2026-05-31 22:37:27 +0800

    chore: Commit changes

--- src/app/scroll-top/scroll-top.component.ts
+++ src/app/scroll-top/scroll-top.component.ts
@@ -1,5 +1,5 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, OnInit, Inject, HostListener } from '@angular/core';
+
+import { Component, OnInit, Inject, HostListener, DOCUMENT } from '@angular/core';
 
 @Component({
     selector: 'app-scroll-top',
